c79ad8110f14d150ac56155ae22b053e4c0f5d10
Neil McGibbon <code@neilmcgibbon.com>
2024-02-28 17:15:19 +0000

    feat: initial commit

--- iam-role-aws-load-balancer-controller/aws_iam_role_policy.tf
+++ iam-role-aws-load-balancer-controller/aws_iam_role_policy.tf
@@ -5,6 +5,6 @@
 ################################################################################
 resource "aws_iam_role_policy" "default" {
   name   = "default-policy"
-  role       = aws_iam_role.this.name
-  policy                = data.aws_iam_policy_document.default.json
+  role   = aws_iam_role.this.name
+  policy = data.aws_iam_policy_document.default.json
 }
